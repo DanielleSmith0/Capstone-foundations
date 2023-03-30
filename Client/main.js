b2b4a97164d1b400481811e93fb82af2b3e2512f
DanielleSmith0 <Danielahsmith@icloud.com>
2023-03-30 16:53:22 -0600

    front-end continued

---
+++ Client/main.js
@@ -0,0 +1,12 @@
+const getRandomWordBtn = document.getElementById("ret-random-word-button");
+
+const getWordListBtn = document.getElementById("get-word-list-button");
+
+const correctBtn = document.getElementById("correct-button");
+const practiceBtn = document.getElementById("practice-button");
+
+const createWordBtn = document.getElementById("createWordButton");
+
+const addToCustomBtn = document.querySelector(".addToCustomList");
+
+const removeWordBtn = document.querySelector(".removeWordButton");
